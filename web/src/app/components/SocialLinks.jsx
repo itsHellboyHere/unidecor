bab51b2da5c2ab079e96191cc3a554c78e166a45
Vishal Kumar <visalkr976@gmail.com>
2026-05-03 16:24:56 +0530

    update

--- web/src/app/components/SocialLinks.jsx
+++ web/src/app/components/SocialLinks.jsx
@@ -1,18 +1,35 @@
 "use client";
 
-import { FaFacebook ,FaWhatsapp, FaInstagram} from "react-icons/fa";
+import { FaFacebook, FaWhatsapp, FaInstagram } from "react-icons/fa";
 import styles from "@/app/css/SocialLinks.module.css";
 
 export default function SocialLinks() {
   return (
     <div className={styles.socials}>
-      <a href="https://www.instagram.com/unidecorofficial?igsh=MWd2czRhNmx5Ym1yOA==" aria-label="Instagram">
+      <a
+        href="https://www.instagram.com/unidecorofficial?igsh=MWd2czRhNmx5Ym1yOA=="
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Instagram"
+      >
         <FaInstagram size={18} />
       </a>
-      <a href="https://www.facebook.com/profile.php?id=61576452005906#" aria-label="Facebook">
+
+      <a
+        href="https://www.facebook.com/profile.php?id=61576452005906#"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Facebook"
+      >
         <FaFacebook size={18} />
       </a>
-      <a href="https://wa.me/918527355586" target="_blank"  rel="noopener noreferrer" aria-label="WhatsApp">
+
+      <a
+        href="https://wa.me/918527355586"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="WhatsApp"
+      >
         <FaWhatsapp size={18} />
       </a>
     </div>
